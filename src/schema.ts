f818b46909f17924301c0dbd2da343aabe99f415
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-15 16:52:42 +0900

    feat: GraphQL Schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,37 @@
-import { gql } from "apollo-server-fastify";
+import { gql } from 'apollo-server-fastify';
 
 const typeDefs = gql`
-    type Query {
-        teamMember: [Person]
-    }
+  type Query {
+    teamMember: [Person]
+  }
 
-    type Person {
-        name: String!
-        age: Int
-    }
+  type Person {
+    name: String!
+    age: Int
+  }
+
+  type User {
+    userId: Int
+    nickname: String
+    email: String
+    apiKey: String
+  }
+  type Repo {
+    repoId: Int
+    userId: Int
+    repoName: String
+    repoUrl: String
+    repoBranch: String
+    deployRepo: String
+    state: boolean
+  }
+
+  type History {
+    id: Int
+    repoId: Int
+    date: String
+    state: String
+  }
 `;
 
 export default typeDefs;
