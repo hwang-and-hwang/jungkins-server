d7b94b06d6b15cb5b222d4d6d3f0be756f506e42
imnotmoon <imnotmoon@gmail.com>
2022-05-17 01:47:07 +0900

    refactor : refactor resolvers

--- src/resolvers/repository.ts
+++ src/resolvers/repository.ts
@@ -1,9 +1,13 @@
-import { Repository } from '../models/types';
-import { repoList } from '../__db__/mock';
+import { Repository } from '../models/Repository';
+import { get, tableNames } from "../__db__";
+
+const REPOSITORY: tableNames = 'Repository';
 
 const repo = {
-  repo: (params: any, args: { id: string }, context: any, info: any) =>
-    repoList.filter((repo: Repository) => repo.id === args.id)[0],
+  repo: async (params: any, args: { id: string }) => {
+    const result = await get({ table: REPOSITORY });
+    return result.filter((repo: Repository) => repo.id === args.id)[0];
+  }
 };
 
 export default repo;
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { User } from '../models/types';
+import { User } from '../models/User';
 import { get, tableNames } from "../__db__";
 
 const USER: tableNames = 'User';
