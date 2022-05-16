b90b112585828911f06f7ba406358407306c1648
imnotmoon <imnotmoon@gmail.com>
2022-05-17 01:41:47 +0900

    feat : add cors allowed hostname

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import fastify from 'fastify';
 import { ApolloServer } from 'apollo-server-fastify';
+import cors from '@fastify/cors';
 import resolvers from './resolvers';
 import typeDefs from './schema';
 
 const PORT = 5000;
 const app = fastify();
 const server = new ApolloServer({ typeDefs, resolvers });
+
+app.register(cors, {
+  origin: (origin, cb) => {
+    const hostname = new URL(origin).hostname;
+    const allowedHostnameRegEx = /(localhost|127.0.0.[\d])/gi;
+    if(!allowedHostnameRegEx.test(hostname)) {
+      cb(null, true);
+      return;
+    }
+    cb(new Error("Not Allowed"), false);
+  }
+});
 
 app.get('/ping', async (request, reply) => {
   return 'pong\n';
