be02414eef8f5299175b3c1618978113cdb35fc5
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-17 20:56:52 +0900

    error: cors 설정 오류 임시방편

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,17 @@ const PORT = 5000;
 const app = fastify();
 const server = new ApolloServer({ typeDefs, resolvers });
 
-app.register(cors, {
-  origin: (origin, cb) => {
-    const hostname = new URL(origin).hostname;
-    const allowedHostnameRegEx = /(localhost|127.0.0.[\d])/gi;
-    if(allowedHostnameRegEx.test(hostname)) {
-      cb(null, true);
-      return;
-    }
-    cb(new Error("Not Allowed"), false);
-  }
-});
+// app.register(cors, {
+//   origin: (origin, cb) => {
+//     const hostname = new URL(origin).hostname;
+//     const allowedHostnameRegEx = /(localhost|127.0.0.[\d])/gi;
+//     if(allowedHostnameRegEx.test(hostname)) {
+//       cb(null, true);
+//       return;
+//     }
+//     cb(new Error("Not Allowed"), false);
+//   }
+// });
 
 app.get('/ping', async (request, reply) => {
   return 'pong\n';
@@ -26,7 +26,13 @@ app.get('/ping', async (request, reply) => {
 
 (async function () {
   await server.start();
-  app.register(server.createHandler());
+  app.register(
+    server.createHandler({
+      cors: {
+        origin: true,
+      },
+    }),
+  );
   await app.listen(PORT, (err, address) => {
     if (err) {
       console.error(err);
