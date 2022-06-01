6bdc3fbb09f523342efaecc1d62b8fe4019a07fe
KIM CHAN HEE <superlipbalm@gmail.com>
2022-06-01 17:42:23 +0900

    fix : User 스키마 수정

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = new mongoose.Schema({
-  title: String,
-  brand: String,
-  price: String,
+  name: String,
   age: Number,
   user_id: ObjectId,
 });
