fb3c70bff1b48587a3d915ff7c6f542f672bb93d
KIM CHAN HEE <superlipbalm@gmail.com>
2022-06-01 19:49:15 +0900

    feat : gql schema 추가

---
+++ src/schema/index.ts
@@ -0,0 +1,56 @@
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+import userController from '../controllers/userController';
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    user_id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: userType,
+      args: { id: { type: GraphQLID } },
+      async resolve(parent, args) {
+        return await userController.getUser(args);
+      },
+    },
+  },
+});
+
+const Mutations = new GraphQLObjectType({
+  name: 'Mutations',
+  fields: {
+    addUser: {
+      type: userType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        user_id: { type: GraphQLID },
+      },
+      async resolve(args) {
+        const data = await userController.addUser(args);
+        return data;
+      },
+    },
+  },
+});
+
+export default new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutations,
+});
