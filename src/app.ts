1914f20b7512a068c91a75adc61ac24908f397a7
KIM CHAN HEE <superlipbalm@gmail.com>
2022-06-01 17:59:26 +0900

    fix : mongodb uri export

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,12 @@ export type AppOptions = {
   // Place your custom options for app below here.
 } & Partial<AutoloadPluginOptions>;
 
-const uri =
+export const uri =
   'mongodb+srv://hyunmindev:R2VFjGLXzAQNJ3BX@cluster0.8nrnd.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(uri).catch((err) => console.log(err));
-
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
+  await mongoose.connect(uri).catch((err) => console.log(err));
+
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
     options: opts,
